7fc221bc0c1f8d1f7976dcc17591af3d591a389e
Otger Ballester <otger@ifae.es>
2017-03-31 16:00:27 +0200

    Simplified controller Improved test, although not a unit test yet

--- pidcontroller/pid.py
+++ pidcontroller/pid.py
@@ -2,61 +2,52 @@ import time
 import math
 
 
-class PID(object):
-    def __init__(self, kp, ki, kd, reverse=False, min_out=0, max_out=100):
-        self.last_input = 0
+class PIDController(object):
+    """
+    Simple PID Controller
+    """
+    def __init__(self, kp, ki=0, kd=0, setpoint=0, output_range=(0, 100)):
+        self.last_in_value = 0
         self.output = 0
-        self.setpoint = 0
-        self.kp = 0
-        self.ki = 0
-        self.kd = 0
-        self.reverse = reverse
-        self.set_tunings(kp, ki, kd)
+        self.setpoint = setpoint
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
 
         self.last_update = time.time()
 
-        self.max_out = max_out
-        self.min_out = min_out
+        self.max_out = output_range[1]
+        self.min_out = output_range[0]
 
         self.enabled = True
 
-    def set_setpoint(self, setpoint):
-        self.setpoint = setpoint
+        self.terms = [0, 0, 0]
+
+    def _set_setpoint(self, setpoint):
+        self._setpoint = setpoint
+
+    def _get_setpoint(self):
+        return self._setpoint
+    setpoint = property(_get_setpoint, _set_setpoint)
+
+    def _clamp_term(self, value):
+        return max(min(value, self.max_out), self.min_out)
 
     def update(self, in_value):
-        if not self.enabled:
-            return 0
         now = time.time()
         delta_time = now - self.last_update
-        d_input = in_value - self.last_input
+        input_value = in_value - self.last_in_value
         error = self.setpoint - in_value
-        iterm = self.ki*error*delta_time
-        iterm = max(min(iterm, self.max_out), self.min_out)
+        self.terms[0] = self.kp*error
+        self.terms[1] = self.ki*error*delta_time
+        self.terms[2] = self.kd*input_value/delta_time
+        out = self.terms[0] + self.terms[1] + self.terms[2]
+        self.output = self._clamp_term(out)
 
-        out = self.kp*error + iterm - self.kd*d_input/delta_time
-        self.output = max(min(out, self.max_out), self.min_out)
-
-        self.last_input = in_value
+        self.last_in_value = in_value
         self.last_update = now
+
+        if not self.enabled:
+            return 0
         return self.output
 
-    def set_tunings(self, kp=None, ki=None, kd=None):
-        self.kp = math.fabs(kp or self.kp)
-        self.ki = math.fabs(ki or self.ki)
-        self.kd = math.fabs(kd or self.kd)
-
-        if self.reverse:
-            self.kp = -self.kp
-            self.ki = -self.ki
-            self.kd = -self.kd
-
-    def enable(self):
-        self.enabled = True
-
-    def disable(self):
-        self.enabled = False
-
-    def set_direction(self, reverse=False):
-        """When a bigger output makes decrease input, it is a reverse system"""
-        self.reverse = reverse
-        self.set_tunings()
--- test/devices.py
+++ test/devices.py
@@ -4,16 +4,16 @@
 # @Author  : Otger Ballester (otger@ifae.es)
 import time
 import threading
-from pidcontroller.pid import PID
+from pidcontroller.pid import PIDController
 
 
 class System(object):
-    def __init__(self, init_temp=275, temp_amb=295, specific_heat=4.1868, mass=10):
+    def __init__(self, init_temp=275, temp_amb=295, specific_heat=4.1868, mass=10, update_time=0.1):
         self.temp = init_temp
         self.temp_amb = temp_amb
         self.c = specific_heat
         self.m = mass
-        self.w = 100
+        self.actuator_power = 100
         self.percent = 0
         self.w_applied = 0
         self.lost_heat = 0
@@ -21,6 +21,7 @@ class System(object):
         self.delta_time = 0
         self.last_update = time.time()
         self.timer = None
+        self.update_time = update_time
 
     def update(self):
         now = time.time()
@@ -28,17 +29,16 @@ class System(object):
         self.last_update = now
 
         self.lost_heat = 0.1*(self.temp - self.temp_amb)*self.delta_time
-        self.applied_heat = -self.w_applied * self.delta_time
+        self.applied_heat = self.w_applied * self.delta_time
 
         self.temp += (self.applied_heat - self.lost_heat)/(self.m*self.c)
 
-        self.timer = threading.Timer(0.1, self.update)
-        #print(str([now, self.lost_heat, self.applied_heat]))
+        self.timer = threading.Timer(self.update_time, self.update)
         self.timer.start()
 
-    def apply_heat(self, percent):
+    def change_actuator(self, percent):
         self.percent = percent
-        self.w_applied = self.w * percent/100
+        self.w_applied = self.actuator_power * percent / 100
 
     def start(self):
         if self.timer is None:
@@ -46,19 +46,30 @@ class System(object):
 
 
 if __name__ == "__main__":
-    s = System()
-    s.start()
-    pid = PID(kp=5, ki=0.2, kd=1.5)
-    pid.set_setpoint(200)
-    pid.set_direction(reverse=True)
+    cooler_system = System()
+    cooler_system.actuator_power = -50  # negative, because it is a cooler
+    cooler_system.start()
 
+    cooler_pid = PIDController(kp=-5, ki=-3, kd=-3)  # negative values because it is a cooler
+    cooler_pid.setpoint = 200
+
+    heater_system = System()
+    heater_system.actuator_power = 100
+    heater_system.start()
+    heater_pid = PIDController(kp=5, ki=3, kd=3)
+    heater_pid.setpoint = 300
 
     def update():
-        output = pid.update(s.temp)
-        s.apply_heat(output)
-        print('{0} - {1} - {2}'.format(time.time(), output, str([s.temp, s.applied_heat, s.lost_heat, s.delta_time])))
+        cooler_system.change_actuator(cooler_pid.update(cooler_system.temp))
+        heater_system.change_actuator(heater_pid.update(heater_system.temp))
+        print('{0:.2f} - {1:.2f} ({2:.1f} + {3:.1f} + {4:.1f}) -  {5:.2f} - {6:.2f}  ({7:.1f} + {8:.1f} + {9:.1f})'.format(
+            cooler_system.temp, cooler_system.percent, cooler_pid.terms[0], cooler_pid.terms[1], cooler_pid.terms[2],
+            heater_system.temp, heater_system.percent, heater_pid.terms[0], heater_pid.terms[1], heater_pid.terms[2]))
         timer = threading.Timer(2, update)
         timer.start()
 
     update()
     time.sleep(100)
+    print('Change setpoints')
+    cooler_pid.setpoint = 250
+    heater_pid.setpoint = 295
